feacfb17c0e025250d5bd19a1ded2ab0a70dfc05
Denise <denise.ytlin@gmail.com>
2021-03-03 05:32:06 +0000

    Complete Task 1,2,3

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,7 +6,7 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -50,7 +50,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id)
     print(melon)
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -95,12 +95,36 @@ def add_to_cart(melon_id):
     #
     # - check if a "cart" exists in the session, and create one (an empty
     #   dictionary keyed to the string "cart") if not
+    if "cart" in session:
+        cart = session["cart"]
+    else:
+        cart = session["cart"] = {}
+
     # - check if the desired melon id is the cart, and if not, put it in
     # - increment the count for that melon id by 1
-    # - flash a success message
-    # - redirect the user to the cart page
+    if melon_id in cart:
+        cart[melon_id] += 1
+    else:
+        cart[melon_id] = 1
 
-    return "Oops! This needs to be implemented!"
+    # if word in word_counts:
+    #         word_counts[word] += 1
+    #     else:
+    #         word_counts[word] = 1
+
+   
+
+    # - flash a success message
+    flash("Success added!")
+
+    # - redirect the user to the cart page
+    return redirect("/cart")
+
+
+    # return render_template("cart.html")
+
+
+
 
 
 @app.route("/login", methods=["GET"])
